a185a1815752cf8b9902e8a376362d971b9f75a2
Sobin Johnson <103770437+SobinJohnson@users.noreply.github.com>
2022-04-30 15:26:52 +0530

    Comment added for _getch and variable explaination

--- gpa_cal.c
+++ gpa_cal.c
@@ -30,9 +30,10 @@ void main()
 
 		resgpa += (float)grade * cred;
 		totcred += cred;
-
 	}
 	resgpa = resgpa / totcred;
 	printf("\nGPA obtained in this semester is : %.2f", resgpa);
 	_getch();
+	
+	// _getch used in place of getch because getch name is deprecated and gives an error C4996
 }
