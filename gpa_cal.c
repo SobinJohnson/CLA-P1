f7ad1e753272ffdc99124b32da1cad138470c2df
Sobin Johnson <103770437+SobinJohnson@users.noreply.github.com>
2022-04-30 15:02:47 +0530

    Update gpa_cal.c

--- gpa_cal.c
+++ gpa_cal.c
@@ -1,3 +1,6 @@
+//RA2111003010137
+//SOBIN JOHNSON
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <conio.h>
@@ -5,7 +8,16 @@
 void main()
 {
 	int cred, grade, numsub, totcred = 0;
+	
+	//cred     =  credits for that subject
+	//grade    =  grades attained in that subject
+	//numsub   =  number of subjects
+	//totcred  =  total credits 
+	
 	float resgpa = 0;
+	
+	//resgpa   =  total gpa attained in that semester
+	
 	printf("* Welcome to GPA Calculator *\n\nThis calculator is used to calculate your grade point for the semester\nbased on the credits for the subject and marks obtained\n");
 	printf("\n\nEnter the number of subjects :");
 	scanf("%d", &numsub);
